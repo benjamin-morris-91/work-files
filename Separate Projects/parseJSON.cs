1702d5d9da78288933d9a177b15391b1df75b1e5
Benjamin Morris <47235306+benjamin-morris-91@users.noreply.github.com>
2020-01-15 11:22:05 -0600

    added file to parse JSON from ntegrated call reports

---
+++ Separate Projects/parseJSON.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using System.IO;
+
+
+namespace SandBox
+{
+	class Program
+	{
+        static void Main()
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\ben\Desktop\benTest1.txt");
+            String[] s1 = new String[9];
+            int indexOfCDR = 0;
+            int indexOfColon = 0;
+            int diffBetweenCDRandColon = 0;
+            int i = 0;
+            string toEnter;
+
+
+            text = text.Remove(0, 2); //Removes the first two characters
+            text = text.Replace("\\", ""); //Removes the backslashes
+
+            //Will enter loop if there is a matching cdr and will not exit until they have all been removed.
+            while (text.IndexOf("cdr") != -1)
+            {
+                indexOfCDR = text.IndexOf("cdr") - 1; //-1 so it heads to the front of cdr...
+                indexOfColon = text.IndexOf(":", indexOfCDR) + 1; //+1 so it grabs the final :, two parameters so it starts where the correct indexOfCDR is.
+                Console.WriteLine(indexOfCDR);
+                Console.WriteLine(indexOfColon);
+                diffBetweenCDRandColon = indexOfColon - indexOfCDR;
+                text = text.Remove(indexOfCDR, diffBetweenCDRandColon);
+            }
+            text = text.Insert(0, "[");
+            text = text.Insert(text.Length, "]");
+
+            File.WriteAllText(@"C:\Users\ben\Desktop\benTest2.txt", text);
+
+
+            //Need to split up the file into several pastable sections
+            //Console.WriteLine("The total length of the file is: {0}", text.Length);
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+        }
+       
+    }
+}
